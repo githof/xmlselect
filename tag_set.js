ea0fd9df40230b8d37149a111b4364742abe8313
Christophe Prieur <prieur@liafa.fr>
2016-02-18 18:44:14 +0100

    tag_set.js (depuis buenosaires)

---
+++ tag_set.js
@@ -0,0 +1,8 @@
+
+var wedding_tags = { 'epoux': 
+		     ["prenom", "nom", "condition",
+		      "veuf", "pere", "mere"],
+		     'epouse': 
+		     ["prenom", "nom", "condition",
+		      "veuve", "pere", "mere"],
+		   };
