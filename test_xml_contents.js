57bbcd63e2b6f634900d857fb6b3c1b2510c8038
cedeify <cedeify@hotmail.fr>
2016-02-19 19:37:06 +0100

    Test de xml_contents sur les exemples epoux et epouse. Ajout de getter pour xml_node. Ajout d'une methode pretty_print pour faciliter les futures debugs.

--- test_xml_contents.js
+++ test_xml_contents.js
@@ -6,8 +6,39 @@ $(document).ready(function (){
     var texte_epoux = "Felipe José de los SANTOS, natural de Lisboa, hijo legítimo de Pedro José de los Santos, y de Francisca Angélica Almeida";
     var texte_epouse = "María Sinforosa SUAREZ, natural de ésta, hija legítima de Pedro Suárez, y de María Bernardina Chavarría. Ts.: Manuel Argerich, y Rufina Marín, (f. 62v)."
 
+    /*
     var xml = new xml_contents()
 	.add_tag_text("epoux", texte_epoux)
 	.add_tag_text("epouse", texte_epouse);
+	*/
 
+    var xml = new xml_contents()
+        .add_node("epoux", new xml_contents()
+            .add_tag_text("prenom", "Felipe José")
+            .add_tag_text("nom", "de los Santos")
+            .add_text(", natural de Lisboa, hijo legítimo de ")
+            .add_node("pere", new xml_contents()
+                .add_tag_text("prenom", "Pedro José")
+                .add_tag_text("nom", "de los Santos"))
+            .add_text(", y de ")
+            .add_node("mere", new xml_contents()
+                .add_tag_text("prenom", "Francisca Angélica")
+                .add_tag_text("nom", "Almedia"))
+        )
+        .add_node("epouse", new xml_contents()
+            .add_tag_text("prenom", "María Sinforosa")
+            .add_tag_text("nom", "SUAREZ")
+            .add_text(", natural de ésta, hija legítima de ")
+            .add_node("pere", new xml_contents()
+                .add_tag_text("prenom", "Pedro")
+                .add_tag_text("nom", "Suárez"))
+            .add_text(", y de ")
+            .add_node("mere", new xml_contents()
+                .add_tag_text("prenom", "María Bernardina")
+                .add_tag_text("nom", "Chavarría"))
+            .add_text(". Ts.: Manuel Argerich, y Rufina Marín, (f. 62v).")
+        );
+
+    //console.log(xml);
+    console.log(xml.pretty_string(""));
 });
--- xml_contents.js
+++ xml_contents.js
@@ -8,22 +8,63 @@ function xml_node()
     this.tag = null;
     this.contents = null;
 
+    /*
+        Initialise un node (tag, contents)
+     */
     this.set_node = function(tag, contents)
     {
-	that.tag = tag;
-	that.contents = contents;
-	return that;
+        that.tag = tag;
+        that.contents = contents;
+        return that;
     }
 
+    /*
+        Initialise un node (null, text)
+     */
     this.set_text_node = function(text)
     {
-	return that.set_node(null, text);
+	    return that.set_node(null, text);
     }
 
+    /*
+        Initialise un node (tag, text)
+     */
     this.set_tag_text_node = function(tag, text)
     {
-	var text_node = new xml_node().set_text_node(text);
-	return that.set_node(tag, text_node);
+        var text_node = new xml_node().set_text_node(text);
+        return that.set_node(tag, text_node);
+    }
+
+    /*
+        Getter tag
+     */
+    this.get_tag = function()
+    {
+        return that.tag;
+    }
+
+    /*
+        Getter contents
+     */
+    this.get_contents = function()
+    {
+        return that.contents;
+    }
+
+    /*
+        Getter text
+     */
+    this.get_text = function()
+    {
+        return that.get_contents();
+    }
+
+    this.pretty_string = function(indentation)
+    {
+        if(that.tag == null)
+            return that.contents.toString();
+
+        return that.tag+": "+that.contents.pretty_string(indentation+"   ");
     }
 }
 
@@ -32,28 +73,58 @@ function xml_contents()
     var that = this;
     this.contents = [];
 
+    /*
+        Ajoute un node
+     */
     this.add_xml_node = function(node)
     {
-	that.contents.push(node);
-	return that;
+        that.contents.push(node);
+        return that;
     }
 
+    /*
+        Ajoute un node (tag, contents)
+     */
     this.add_node = function (tag, contents)
     {
-	var node = new xml_node().set_node(tag, contents);
-	return that.add_xml_node(node);
+        var node = new xml_node().set_node(tag, contents);
+        return that.add_xml_node(node);
     }
 
+    /*
+        Ajoute un node (tag, text)
+     */
     this.add_tag_text = function(tag, text)
     {
-	var node = new xml_node().set_tag_text_node(tag, text);
-	return that.add_xml_node(node);
+        var node = new xml_node().set_tag_text_node(tag, text);
+        return that.add_xml_node(node);
     }
 
+    /*
+        Ajoute un node (null, text)
+     */
     this.add_text = function (text)
     {
-	var node = new xml_node().set_text_node(text);
-	return that.add_xml_node(node);
+        var node = new xml_node().set_text_node(text);
+        return that.add_xml_node(node);
+    }
+
+    this.toString = function()
+    {
+        var s = "";
+        that.pretty_string("");
+        return s;
+    }
+
+    this.pretty_string = function(indentation)
+    {
+        var s = "\n";
+        for(var i = 0; i < that.contents.length; i++){
+            s += indentation + that.contents[i].pretty_string(indentation);
+            if(!s.endsWith("\n"))
+                s += "\n";
+        }
+        return s;
     }
 }
 
