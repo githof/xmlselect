8d311a66e826e511d637f88b7387e6fac1c8c4ad
Christophe Prieur <prieur@liafa.fr>
2016-03-15 19:19:57 +0100

    test_acte.js

--- test_taggable_xml.js
+++ test_taggable_xml.js
@@ -1,3 +1,8 @@
+/*
+  needs xml_contents.js
+  needs test_acte.js
+*/
+
 $(document).ready(function (){
     var acte_node = new xml_node().set_node("acte", acte);
     new taggable_text(acte_node).append_to($("#test"));
