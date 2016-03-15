fc10eff2ee7bc4e274f02c47bb3eeb4dd07fa548
Christophe Prieur <prieur@liafa.fr>
2016-03-15 17:43:26 +0100

    test is

--- test_xml_contents.js
+++ test_xml_contents.js
@@ -9,15 +9,19 @@ $(document).ready(function (){
     console.log('## Tests plus ou moins unitaires');
 
     var texte_epoux = "Felipe José de los SANTOS, natural de Lisboa, hijo legítimo de Pedro José de los Santos, y de Francisca Angélica Almeida";
-    var texte_epouse = "María Sinforosa SUAREZ, natural de ésta, hija legítima de Pedro Suárez, y de María Bernardina Chavarría. Ts.: Manuel Argerich, y Rufina Marín, (f. 62v)."
+    var texte_epouse = "María Sinforosa SUAREZ, natural de ésta, hija legítima de Pedro Suárez, y de María Bernardina Chavarría"
 
     var n_text = new xml_node().set_text_node(texte_epoux);
     console.log('n_text');
     console.log(n_text.toString());
+    console.log('is?');
+    console.log(n_text.is_text_node());
 
     var n_tag_text = new xml_node().set_tag_text_node('epouse', texte_epouse);
     console.log('n_tag_text');
     console.log(n_tag_text.toString());
+    console.log('is?');
+    console.log(n_tag_text.is_tag_text_node());
 
     /*
     var xml = new xml_contents()
