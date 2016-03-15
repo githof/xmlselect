369a1dd9668eb9dd496a078fb29f8868654238c6
Christophe Prieur <prieur@liafa.fr>
2016-03-15 17:34:07 +0100

    fix: unduplicate names

--- test_xml_contents.js
+++ test_xml_contents.js
@@ -11,13 +11,13 @@ $(document).ready(function (){
     var texte_epoux = "Felipe José de los SANTOS, natural de Lisboa, hijo legítimo de Pedro José de los Santos, y de Francisca Angélica Almeida";
     var texte_epouse = "María Sinforosa SUAREZ, natural de ésta, hija legítima de Pedro Suárez, y de María Bernardina Chavarría. Ts.: Manuel Argerich, y Rufina Marín, (f. 62v)."
 
-    var text_node = new xml_node().set_text_node(texte_epoux);
-    console.log('text_node');
-    console.log(text_node.toString());
+    var n_text = new xml_node().set_text_node(texte_epoux);
+    console.log('n_text');
+    console.log(n_text.toString());
 
-    var tag_text_node = new xml_node().set_tag_text_node('epouse', texte_epouse);
-    console.log('tag_text_node');
-    console.log(tag_text_node.toString());
+    var n_tag_text = new xml_node().set_tag_text_node('epouse', texte_epouse);
+    console.log('n_tag_text');
+    console.log(n_tag_text.toString());
 
     /*
     var xml = new xml_contents()
