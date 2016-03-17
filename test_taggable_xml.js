bb0db16c65fe470394d4b4019c142b88d95f76f0
cedeify <cedeify@hotmail.fr>
2016-03-17 10:21:07 +0100

    test: taggable_xml

--- test_taggable_xml.js
+++ test_taggable_xml.js
@@ -4,6 +4,29 @@
 */
 
 $(document).ready(function (){
+
+    // Test pour text_node
+    //var text_node = new xml_node().set_text_node("test text node");
+    //new taggable_xml(text_node).append_to($("#test"));
+
+
+    // Test pout tag_text_node
+    //var tag_text_node = new xml_node().set_tag_text_node("TAG TAG", "text text text");
+    //new taggable_xml(tag_text_node).append_to($("#test"));
+
+
+    // Test xml_contents
+    /*
+    var node = new xml_node().set_node("pere", new xml_contents()
+        .add_tag_text("prenom", "Pedro")
+        .add_tag_text("prenom", "José")
+        .add_tag_text("nom", "de los Santos")
+    );
+    new taggable_xml(node).append_to($("#test"));
+    */
+
+
+    // Test global sur un acte
     var acte = test_acte();
     var acte_node = new xml_node().set_node("acte", acte);
     new taggable_xml(acte_node).append_to($("#test"));
