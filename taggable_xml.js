071959e8db787688703713501f4397a4d9894ed9
cedeify <cedeify@hotmail.fr>
2016-03-17 08:54:29 +0100

    add $ to jquery var

--- taggable_xml.js
+++ taggable_xml.js
@@ -13,79 +13,79 @@ function taggable_xml (xml)
 
     this.set_html_xml_node = function()
     {
-        var ul = $("<ul>",
+        var $ul = $("<ul>",
             {
                 'class': 'xml_contents'
             });
 
-        var dd = $("<dd>", {});
+        var $dd = $("<dd>", {});
 
-        var dt = $("<dt>",
+        var $dt = $("<dt>",
             {
                 text: that.xml.get_tag()
             });
 
-        dd.append(ul);
+        $dd.append($ul);
 
-        that.$element = [dt, dd];
+        that.$element = [$dt, $dd];
     }
 
     this.set_html_tag_text_node = function()
     {
-        var dt = $("<dt>", {
+        var $dt = $("<dt>", {
             text: that.xml.get_tag()
         });
 
-        var dd = $("<dd>", {});
+        var $dd = $("<dd>", {});
 
-	var tg_text = new taggable_xml(that.xml.get_text_node());
-	dd.append(tg_text.$element);
+	    var tg_text = new taggable_xml(that.xml.get_text_node());
+	    $dd.append(tg_text.$element);
 
-        that.$element = [dt, dd];
+        that.$element = [$dt, $dd];
     }
 
     this.set_html_text_node = function()
     {
-        var p_source = $("<p>",
+        var $p_source = $("<p>",
             {
                 'class': 'xml_text source',
                 text: that.xml.get_text()
             });
 
-        var button_ok = $("<button>",
+        var $button_ok = $("<button>",
             {
                 text: 'Ok'
             });
 
-        var form_tag_choice = $("<form>",
+        var $form_tag_choice = $("<form>",
             {
-                'class': 'tag_choice',
+                'class': 'tag_choice'
             });
 
-        var p_show = $("<p>",
+        var $p_show = $("<p>",
             {
                 'class': 'xml_text show'
             });
 
-        var section_panel = $("<section>",
+        var $section_panel = $("<section>",
             {
-                'class': 'selection_panel',
+                'class': 'selection_panel'
             });
 
-        var section_text_node = $("<section>",
+        var $section_text_node = $("<section>",
             {
-                'class': 'text_node',
+                'class': 'text_node'
             });
 
         var choices = new radio_list(choice_tags, 'ID');
-        form_tag_choice.append(choices.$element());
+        $form_tag_choice.append(choices.$element());
 
-        section_panel.append(form_tag_choice);
-        section_panel.append(p_show);
+        $section_panel.append($form_tag_choice);
+        $section_panel.append($p_show);
 
-        section_text_node.append(p_source);
-        section_text_node.append(button_ok);
-        section_text_node.append(section_panel);
+        $section_text_node.append($p_source);
+        $section_text_node.append($button_ok);
+        $section_text_node.append($section_panel);
 
         that.$element = section_text_node;
     }
