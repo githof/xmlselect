00b3d327c648adf01936c4bed882f7e3566cebcd
cedeify <cedeify@hotmail.fr>
2016-03-24 21:19:39 +0100

    update wedding_tags

--- tag_set.js
+++ tag_set.js
@@ -1,20 +1,27 @@
 
-var wedding_tags = { 'epoux': 
-		     ["prenom", "nom", "condition",
-		      "veuf", "pere", "mere"],
-		     'epouse': 
-		     ["prenom", "nom", "condition",
-		      "veuve", "pere", "mere"],
-		   };
-
-var choice_tags = [
-	"epoux",
-	"epouse",
-	"prenom",
-	"nom",
-	"condition",
-	"veuf",
-	"veuve",
-	"pere",
-	"mere"
-];
+var wedding_tags = {
+	'epoux': [
+		"prenom", "nom", "condition", "veuf", "pere", "mere"
+	],
+	'epouse': [
+		"prenom", "nom", "condition", "veuve", "pere", "mere"
+	],
+	'pere': [
+		"prenom", "nom"
+	],
+	'mere': [
+		"prenom", "nom"
+	],
+	'temoins': [
+		"temoin"
+	],
+	'temoin': [
+		"prenom", "nom"
+	],
+	'veuf': [
+		"prenom", "nom"
+	],
+	'veuve': [
+		"prenom", "nom"
+	]
+};
