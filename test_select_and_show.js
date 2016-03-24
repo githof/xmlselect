4f8c45ea3638c41d0b347a51e9ea36eccb44da8f
cedeify <cedeify@hotmail.fr>
2016-03-24 13:12:01 +0100

    test select_and_show

--- test_select_and_show.js
+++ test_select_and_show.js
@@ -1,42 +1,29 @@
 $(document).ready(function(){
 
-    var $source = $("#acte");
-    var $show = $("#sel");
+	// Test pour text_node
+	//var text_node = new xml_node().set_text_node("Prenom Nom");
+	//new taggable_xml(text_node, 'text_node').append_to($("#test"));
 
-    var S = new select_and_show(
-	$source,
-	$show, 
-	function ()
-	{
-	    console.log('before: ' + this.before);
-	    console.log('selection: ' + this.select);
-	    console.log('after: ' + this.after);
-	}
-    );
 
-    $source = $('<p>',
-		     {
-			 'class': "texte",
-		     });
+	// Test pout tag_text_node
+	//var tag_text_node = new xml_node().set_tag_text_node("epoux", "Nom Prenom");
+	//new taggable_xml(tag_text_node, 'tag_text_node').append_to($("#test"));
 
-    $show = $('<p>',
-		     {
-			 'class': "texte",
-		     });
 
-    $("#test").append([ $source, $show ]);
+	// Test xml_contents
+	// var node = new xml_node().set_node("pere", new xml_contents()
+	//	 .add_tag_text("prenom", "Pedro")
+	//	 .add_tag_text("prenom", "José")
+	//	 .add_tag_text("nom", "de los Santos")
+	//	 .add_text("text sans tag")
+	// );
+	// new taggable_xml(node, 'node').append_to($("#test"));
 
-    $source.text("salut Tal, c'est encore moi !");
-    
-    var S2 = new select_and_show(
-	$source,
-	$show,
-	function ()
-	{
-	    console.log('before: ' + this.before);
-	    console.log('selection: ' + this.select);
-	    console.log('after: ' + this.after);
-	}
-    );
+
+
+	// Test global sur un acte
+	var acte = test_acte();
+	var acte_node = new xml_node().set_node("acte", acte);
+	new taggable_xml(acte_node, 'acte').append_to($("#test"));
 
 });
