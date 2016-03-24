f5041eb21a4ab42c691beb4d29322ff6b875cd20
cedeify <cedeify@hotmail.fr>
2016-03-24 21:25:38 +0100

    test: weddings_tags

--- test_acte.js
+++ test_acte.js
@@ -48,3 +48,51 @@ function test_acte()
 
     return acte;
 }
+
+function test_acte_incomplet()
+{
+    var acte = new xml_contents()
+        .add_node("epoux", new xml_contents()
+            .add_text("Felipe José")
+            .add_tag_text("nom", "de los Santos")
+            .add_text(", natural de Lisboa, hijo legítimo de ")
+            .add_node("pere", new xml_contents()
+                .add_text("Pedro José de los Santos"))
+            .add_text(",_y_de_")
+            .add_node("mere", new xml_contents()
+                .add_tag_text("prenom", "Francisca")
+                .add_tag_text("prenom", "Angélica")
+                .add_tag_text("nom", "Almedia"))
+        )
+        .add_node("epouse", new xml_contents()
+            .add_text("Maria Sinforosa SUAREZ")
+            .add_text(", natural de ésta, hija legítima de_")
+            .add_node("pere", new xml_contents()
+                .add_tag_text("prenom", "Pedro")
+                .add_tag_text("nom", "Suárez"))
+            .add_text(",_y_de_María Bernardina Chavarría")
+        );
+
+    var temoin1 = new xml_node().
+        set_node("temoin", new xml_contents()
+            .add_tag_text("Prenom", "Manuel")
+            .add_tag_text("Nom", "Argerich")
+    );
+
+    var temoin2 = new xml_node().
+        set_node("temoin", new xml_contents()
+        .add_tag_text("temoin", "Rufina Marín")
+    );
+
+    var temoins = new xml_node()
+        .set_node("temoins", new xml_contents()
+            .add_xml_node(temoin1)
+            .add_text(", y ")
+            .add_xml_node(temoin2)
+        );
+
+    acte.add_xml_node(temoins)
+        .add_text(", (f. 62v).");
+
+    return acte;
+}
--- test_taggable_xml.js
+++ test_taggable_xml.js
@@ -27,7 +27,7 @@ $(document).ready(function (){
 
 
     // Test global sur un acte
-    var acte = test_acte();
+    var acte = test_acte_incomplet();
     var acte_node = new xml_node().set_node("acte", acte);
     new taggable_xml(acte_node, 'acte').append_to($("#test"));
 });
