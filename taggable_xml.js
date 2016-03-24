f630471af7a250cc6040bcdb7f3a11d92e4320f5
cedeify <cedeify@hotmail.fr>
2016-03-24 21:24:55 +0100

    add wedding_tags call

--- taggable_xml.js
+++ taggable_xml.js
@@ -2,13 +2,14 @@
   Interface objects for xml_contents
  */
 
-function taggable_xml (xml, id)
+function taggable_xml (xml, id, parent_tag)
 {
     var that = this;
     this.xml = null;
     this.id = id;
     this.$element = null;
     this.duplicate_tags = {};
+    this.parent_tag = parent_tag;
 
     this.get_id = function(tag)
     {
@@ -40,7 +41,7 @@ function taggable_xml (xml, id)
             var $li = $("<li>", {});
             $ul.append($li);
 
-            var fils = new taggable_xml(nodes[i], that.get_id(nodes[i].get_tag()));
+            var fils = new taggable_xml(nodes[i], that.get_id(nodes[i].get_tag()), that.xml.get_tag());
             $li.append(fils.$element);
         }
 
@@ -57,7 +58,7 @@ function taggable_xml (xml, id)
 
         var $dd = $("<dd>", {});
 
-	    var tg_text = new taggable_xml(that.xml.get_text_node(), that.id);
+	    var tg_text = new taggable_xml(that.xml.get_text_node(), that.id, that.xml.get_tag());
 	    $dd.append(tg_text.$element);
 
         that.$element = [$dt, $dd];
@@ -65,51 +66,67 @@ function taggable_xml (xml, id)
 
     this.set_html_text_node = function()
     {
-        var $p_source = $("<p>",
-            {
-                'class': 'xml_text source',
+        var input_tags = wedding_tags[that.parent_tag];
+        console.log(input_tags);
+        if(input_tags == null){
+            var $p_text = $("<p>", {
                 text: that.xml.get_text()
             });
 
-        var $button_ok = $("<button>",
-            {
-                text: 'Ok'
+            var $section_leaf = $("<section>", {
+                'class': 'text_node leaf'
             });
 
-        var $form_tag_choice = $("<form>",
-            {
-                'class': 'tag_choice',
-                'id': that.get_id('form')
-            });
+            $section_leaf.append($p_text);
 
-        var $p_show = $("<p>",
-            {
-                'class': 'xml_text show'
-            });
+            that.$element = $section_leaf;
+        }else {
+            var $p_source = $("<p>",
+                {
+                    'class': 'xml_text source',
+                    text: that.xml.get_text()
+                });
 
-        var $section_panel = $("<section>",
-            {
-                'class': 'selection_panel'
-            });
+            var $button_ok = $("<button>",
+                {
+                    text: 'Ok'
+                });
 
-        var $section_text_node = $("<section>",
-            {
-                'class': 'text_node'
-            });
+            var $form_tag_choice = $("<form>",
+                {
+                    'class': 'tag_choice',
+                    'id': that.get_id('form')
+                });
 
-        var choices = new radio_list(choice_tags, that.get_id('input'));
-        $form_tag_choice.append(choices.$element());
+            var $p_show = $("<p>",
+                {
+                    'class': 'xml_text show'
+                });
 
-        $section_panel.append($form_tag_choice);
-        $section_panel.append($p_show);
+            var $section_panel = $("<section>",
+                {
+                    'class': 'selection_panel'
+                });
 
-        $section_text_node.append($p_source);
-        $section_text_node.append($button_ok);
-        $section_text_node.append($section_panel);
+            var $section_text_node = $("<section>",
+                {
+                    'class': 'text_node'
+                });
 
-        new select_and_show($p_source, $p_show);
+            var choices = new radio_list(input_tags, that.get_id('input'));
+            $form_tag_choice.append(choices.$element());
 
-        that.$element = $section_text_node;
+            $section_panel.append($form_tag_choice);
+            $section_panel.append($p_show);
+
+            $section_text_node.append($p_source);
+            $section_text_node.append($button_ok);
+            $section_text_node.append($section_panel);
+
+            new select_and_show($p_source, $p_show);
+
+            that.$element = $section_text_node;
+        }
     }
 
     this.append_to = function($where)
