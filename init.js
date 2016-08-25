910af3f60b12793ddd11b927d079baa16191d141
cedeify <ceddesgranges@gmail.com>
2016-08-25 16:00:47 +0200

    rename file init.js

---
+++ init.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+    var $html_edit = $(".xmlselect-edit");
+    if($html_edit.length > 0){
+        var acte_brut = $html_edit.text();
+        $html_edit.text("");
+        var acte = new_taggable_xml(xml_parser(acte_brut));
+        acte.append_to($html_edit);
+        $html_edit.parent().children("button").click(function(){
+            console.log(acte.xml.toString());
+        });
+    }
+});
