fec4038afd0f1cab2472c0ab4b33b859ed757394
cedeify <ceddesgranges@gmail.com>
2016-11-05 17:08:27 +0100

    button edit xml acte

--- init.js
+++ init.js
@@ -1,19 +1,26 @@
 
-
-$(document).ready(function(){
+function setup_xmlselect(){
     var $html_edit = $(".xmlselect-edit");
-    if($html_edit.length > 0){
-        var acte_brut = $html_edit.text();
+    var $html_raw_xml = $html_edit.children(".raw-xml");
+    $html_edit.children(".tag-node").remove();
+    if($html_raw_xml.length > 0){
+        var acte_brut = $html_raw_xml.text();
         var is_editable = $html_edit.hasClass("editable");
-        $html_edit.text("");
+        $html_raw_xml.hide();
         var acte = new_taggable_xml(xml_parser(acte_brut), is_editable);
         acte.append_to($html_edit);
         $html_edit.parent().children("button").click(function(){
             console.log(acte.xml.toString());
         });
-        var clipboard = new Clipboard("#btn-copy-xml", {
-            text: function(){
-                return acte.xml.toString();
+    }
+}
+
+$(document).ready(function(){
+        setup_xmlselect();
+
+        var clipboard = new Clipboard("#btn-copy-xml",{
+            text: function(trigger){
+                return $(".raw-xml").text();
             }
         });
         clipboard.on("success", function(e){
@@ -22,6 +29,35 @@ $(document).ready(function(){
                 <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>\
                 XML copié dans le presse-papier</div>")
             );
-        })
-    }
+        });
+
+        $("#btn-edit-xml").click(function(){
+            $(this).hide();
+            $("#btn-copy-xml").hide();
+            $("#btn-save-edit-xml").show();
+            $("#btn-cancel-edit-xml").show();
+
+            $(".xmlselect-edit").addClass("editable");
+            setup_xmlselect();
+        });
+
+        $("#btn-cancel-edit-xml").click(function(){
+            $(this).hide();
+            $("#btn-save-edit-xml").hide();
+            $("#btn-copy-xml").show();
+            $("#btn-edit-xml").show();
+
+            $(".xmlselect-edit").removeClass("editable");
+            setup_xmlselect();
+        });
+
+        $("#btn-save-edit-xml").click(function(){
+            $(this).hide();
+            $("#btn-cancel-edit-xml").hide();
+            $("#btn-copy-xml").show();
+            $("#btn-edit-xml").show();
+
+            $(".xmlselect-edit").removeClass("editable");
+            setup_xmlselect();
+        });
 });
